4ae26a22865c0324ff67cd5a633824e5b179a7c8
Sithara <sitharamurthy@gmail.com>
2018-10-11 00:57:03 -0700

    Assignment2 - Removed Api keys

--- TwitterApi/apps.py
+++ TwitterApi/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class TwitterapiConfig(AppConfig):
     name = 'TwitterApi'
-    consumer_key = "oE5oJ3jDwGSHadbN4BK4yNYuy"
-    consumer_secret = "nmlzHoqkt0tw9hLpFbWWgSzDsEYlfPeVmMlOeybHN84hmJxarn"
-    access_token = "1046137456301506560-4RbRVcUt2FChkrljWF0jD06ikZyaqQ"
-    access_token_secret = "l508SIOvH01kdNSuXTzmwANoZlooDJA66ndRJ8Let5nMK"
+    consumer_key = " "
+    consumer_secret = " "
+    access_token = " "
+    access_token_secret = " "
